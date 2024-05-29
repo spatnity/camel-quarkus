08e662f0ae680d3a28c3f634359c7bdfdc446751
ShravaniP <spatnams@redhat.com>
2024-05-29 16:15:09 +0530

    Testing query with variables

---
+++ integration-tests/graphql/src/main/java/org/apache/camel/quarkus/component/graphql/it/GraphQLRouteBuilder.java
@@ -0,0 +1,22 @@
+package org.apache.camel.quarkus.component.graphql.it;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class GraphQLRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("direct:addBookGraphQL")
+                .toD("graphql://http://localhost:${header.port}/graphql?queryFile=graphql/addBookMutation.graphql&operationName=AddBook");
+
+        from("direct:getBookGraphQL")
+                .toD("graphql://http://localhost:${header.port}/graphql?queryFile=graphql/bookQuery.graphql&operationName=BookById");
+
+        from("direct:getQuery")
+                .toD("graphql://http://localhost:${header.port}/graphql?query={books{id name}}");
+
+        from("direct:addQueryVariables")
+                .toD("graphql://http://localhost:${header.port}/graphql?queryFile=graphql/bookQuery.graphql&variables=#bookByIdQueryVariables");
+    }
+}
